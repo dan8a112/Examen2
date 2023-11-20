6548eb719982298d1392c07449362ea6bb2acf98
Daniel <danyochoa112@gmail.com>
2023-11-20 17:06:24 -0600

    second commit

---
+++ src/main/java/hn/unah/autolote/AutoloteApplication.java
@@ -0,0 +1,13 @@
+package hn.unah.autolote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoloteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoloteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hn/unah/autolote/AutoloteApplicationTests.java
@@ -0,0 +1,13 @@
+package hn.unah.autolote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoloteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
