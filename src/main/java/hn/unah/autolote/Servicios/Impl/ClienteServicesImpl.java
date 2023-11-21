df5bfe053dd3ac0c2e68112c09e049f53325b03b
Daniel <danyochoa112@gmail.com>
2023-11-20 18:06:54 -0600

    crea implementaciones

---
+++ src/main/java/hn/unah/autolote/Servicios/Impl/ClienteServicesImpl.java
@@ -0,0 +1,7 @@
+package hn.unah.autolote.Servicios.Impl;
+
+import hn.unah.autolote.Servicios.ClienteServices;
+
+public class ClienteServicesImpl implements ClienteServices{
+    
+}
---
+++ src/main/java/hn/unah/autolote/Servicios/Impl/VehiculoServicesImpl.java
@@ -0,0 +1,7 @@
+package hn.unah.autolote.Servicios.Impl;
+
+import hn.unah.autolote.Servicios.VehiculoServices;
+
+public class VehiculoServicesImpl implements VehiculoServices {
+    
+}
